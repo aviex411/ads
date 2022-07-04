41c6da1fa4b1aefeeaeca9ffc2de45befac7b057
Aviex <108202612+aviex411@users.noreply.github.com>
2022-07-04 19:42:54 +0700

    Update 300x250.js

--- 300x250.js
+++ 300x250.js
@@ -6,6 +6,5 @@
 		'width' : 300,
 		'params' : {}
 	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://captiveimpossibleimport.com/cf2e6d1d7610db7519353beb4b314861/invoke.js"></scr' + 'ipt>');
 </script>
-
-<script type="text/javascript" src="//captiveimpossibleimport.com/cf2e6d1d7610db7519353beb4b314861/invoke.js"></script>
